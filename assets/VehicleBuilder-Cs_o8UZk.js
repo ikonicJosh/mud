90b6bc46dc4febe245da87aa76d1f7cc956fe916
ikonic <info@ikonicmarketing303.com>
2026-06-30 16:57:16 -0600

    update mud build

--- assets/VehicleBuilder-Cs_o8UZk.js
+++ assets/VehicleBuilder-Cs_o8UZk.js
@@ -1,4 +1,4 @@
-import{r as lt,j as Ce}from"./react-C6IRzGXw.js";import{b as xs}from"./index-BaCgsHnY.js";/**
+import{r as lt,j as Ce}from"./react-C6IRzGXw.js";import{b as xs}from"./index-C0_XoDKZ.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
